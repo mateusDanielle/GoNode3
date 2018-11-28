07b6ad8eaa871dc6602adfccc919b69dbb79a44b
Mateus Danielle <mateusdanielle@edu.fecap.br>
2018-11-28 17:47:19 -0200

    Validação

--- Aulas/src/routes.js
+++ Aulas/src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const validate = require('express-validation')
 
 const routes = express.Router()
 
@@ -7,20 +8,33 @@ const authMiddleware = require('./app/middlewares/auth')
 // const UserController = require('./app/controllers/UserController')
 // const SessionController = require('./app/controllers/SessionController')
 const controllers = require('./app/controllers')
+const validators = require('./app/validators')
 
-routes.post('/users', controllers.UserController.store)
-routes.post('/sessions', controllers.SessionController.store)
+routes.post(
+  '/users',
+  validate(validators.User),
+  controllers.UserController.store
+)
+routes.post(
+  '/sessions',
+  validate(validators.Session),
+  controllers.SessionController.store
+)
 
 routes.use(authMiddleware)
 
 // Ads
 routes.get('/ads', controllers.AdController.index)
 routes.get('/ads/:id', controllers.AdController.show)
-routes.post('/ads', controllers.AdController.store)
-routes.put('/ads/:id', controllers.AdController.update)
+routes.post('/ads', validate(validators.Ad), controllers.AdController.store)
+routes.put('/ads/:id', validate(validators.Ad), controllers.AdController.update)
 routes.delete('/ads/:id', controllers.AdController.destroy)
 
 // Purchase
-routes.post('/purchase', controllers.PurchaseController.store)
+routes.post(
+  '/purchase',
+  validate(validators.Purchase),
+  controllers.PurchaseController.store
+)
 
 module.exports = routes
