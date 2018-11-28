644cd6465f31ecb174810dd3c2aebcb36a6ce10e
Mateus Danielle <mateusdanielle@edu.fecap.br>
2018-11-28 14:26:52 -0200

    Schema de Ad

---
+++ Aulas/src/app/models/Ad.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+const Ad = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  // has many
+  // author: [{
+  //   type: mongoose.Schema.Types.ObjectId,
+  //   ref: 'User',
+  //   required: true
+  // }],
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = mongoose.model('Ad', Ad)
